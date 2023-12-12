9d19fd475cba2d48bdc31cb9c553c2a6ec7a8f7c
mziom <michal.t.partyka@gmail.com>
2023-12-12 20:54:10 +0100

    #8 game scraping

--- code/python/nba.py
+++ code/python/nba.py
@@ -30,7 +30,7 @@ canadian_provinces = [
 primaryKeys = {}
 
 # Open the file in read mode
-with open('../IDMap.txt', 'r') as f:
+with open('../../IDMap.txt', 'r') as f:
     for line in f:
         # Split the line into key and value
         key, value = line.strip().split(': ')
@@ -39,7 +39,7 @@ with open('../IDMap.txt', 'r') as f:
         # Add the key-value pair to the dictionary
         primaryKeys[key] = int(value)
 
-def getPlayerGameInserts(url, gameID):
+def getGameInserts(url, gameID, type = 'regular season game'):
     # Send a GET request to the URL
     response = requests.get(url)
 
@@ -51,7 +51,7 @@ def getPlayerGameInserts(url, gameID):
         # Find every 8th <th> element for "Basic Box Score Stats"
         basic_stats_headers = soup.find_all('th', {'data-stat': 'header_tmp', 'colspan': '20', 'class': 'over_header center'})[::8]
 
-        with open('InsertPlayerGame_' + gameID + '.sql', 'w') as file:
+        with open('../../sqlGenerated/InsertPlayerGame_' + gameID + '.sql', 'w') as file:
             for basic_stats_header in basic_stats_headers:
                 # Navigate to the corresponding <tbody> element for each header
                 tbody = basic_stats_header.find_next('tbody')
@@ -84,6 +84,35 @@ def getPlayerGameInserts(url, gameID):
                             file.write(sql_insert)
                 else:
                     print("No <tbody> element found under 'Basic Box Score Stats'.")
+        # Find the breadcrumbs div
+        breadcrumbs_div = soup.find('div', class_='breadcrumbs')
+
+        if breadcrumbs_div:
+            # Find the strong tag within the breadcrumbs div
+            strong_tag = breadcrumbs_div.find('strong')
+
+            if strong_tag:
+                # Extract and print the text inside the strong tag
+                game_description = strong_tag.get_text(strip=True)
+            else:
+                print("Strong tag not found within breadcrumbs div.")
+            game_description = game_description.split(' at ')
+            away_team = game_description[0].strip()
+            game_description = game_description[1].split(' Box Score, ')
+            home_team = game_description[0].strip()
+            game_date = game_description[1].strip()
+            away_team_id = teams.nba_teams[away_team]
+            home_team_id = teams.nba_teams[home_team]
+        else:
+            print("Breadcrumbs div not found.")  
+
+        with open('../../sqlGenerated/InsertGame_' + gameID + '.sql', 'w') as file:
+            file.write(
+                f"INSERT INTO game "
+                f"(id_game, id_away_team, id_home_team, date, type) "
+                f"VALUES "
+                f"('{gameID}', '{away_team_id}', '{home_team_id}', 'TO_DATE({game_date})', '{type}');\n"
+            )
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
@@ -226,5 +255,5 @@ def getPlayerInfo(url):
 
 
 # Example usage
-#getPlayerGameInserts('https://www.basketball-reference.com/boxscores/202312020CHO.html', '202312020CHO')
+getGameInserts('https://www.basketball-reference.com/boxscores/202312020CHO.html', '202312020CHO')
 #getPlayerInfo("https://www.basketball-reference.com/players/a/antetgi01.html")
